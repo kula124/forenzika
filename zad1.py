d2de27ded0c7c65544d8f7bd4f6baa14a494819b
kula <kula124@gmail.com>
2021-01-04 22:59:41 +0100

    Task 1

---
+++ zad1.py
@@ -0,0 +1,8 @@
+from PIL import Image
+slika = Image.open("slika.png")
+
+w, h = slika.size
+
+watermark = Image.open("watermark.png").resize((w, h))
+skala = 0.3
+Image.blend(slika, watermark, skala).save("watermarked_image.png")
