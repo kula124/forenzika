50224929e0ca6218ffd68cc5190eb36a06fb1f00
kula <kula124@gmail.com>
2021-01-04 21:28:55 +0100

    Task 3

---
+++ zad3.py
@@ -0,0 +1,10 @@
+from PIL import Image
+from PIL import ImageOps
+slika = Image.open("slika.jpg")
+stupci, retci = slika.size
+# 1. na£in - specifikacija novog broja redaka i stupaca
+smanjena_slika_1 = slika.resize((256, 256))
+# 2. na£in - skaliranje (// - integer dijeljenje)
+smanjena_slika_2 = slika.resize((stupci//2, retci//2))
+smanjena_slika_1.save("smanjena_slika_1.jpg")
+smanjena_slika_2.save("smanjena_slika_2.jpg")
